bf550f753c9db71591cfad3378a5fe8e46eea1f6
WSM <wassim99f@gmail.com>
2021-11-03 16:31:52 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,39 @@
-import firebase from "../src/firebase";
-import { getMessaging } from "firebase/messaging/sw";
-importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js");
+// import firebase from "../src/firebase";
+// import { getMessaging } from "firebase/messaging/sw";
+// importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js");
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('../firebase-messaging-sw.js')
+//       .then(function(registration) {
+//         console.log('Registration successful, scope is:', registration.scope);
+//       }).catch(function(err) {
+//        console.log('Service worker registration failed, error:', err);
+//       });
+//     }
+
+// firebase.initializeApp({
+//     messagingSenderId: "1065130398331"
+// });
+
+// const initMessaging = getMessaging(firebase)
+
+
+
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('../firebase-messaging-sw.js')
       .then(function(registration) {
         console.log('Registration successful, scope is:', registration.scope);
       }).catch(function(err) {
-       console.log('Service worker registration failed, error:', err);
+        console.log('Service worker registration failed, error:', err);
       });
     }
 
 firebase.initializeApp({
     messagingSenderId: "1065130398331"
-});
+  })
 
-const initMessaging = getMessaging(firebase)
+const initMessaging = firebase.messaging()
